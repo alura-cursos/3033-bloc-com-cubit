e837550128e4e397118d1c39f30190a2f324f51c
ikyrie <mah.c5r@gmail.com>
2023-05-10 11:14:55 -0300

    video2.2

--- lib/logic/cubit/home_cubit.dart
+++ lib/logic/cubit/home_cubit.dart
@@ -1,8 +1,21 @@
+import 'package:bilheteria_panucci/models/movie.dart';
+import 'package:bilheteria_panucci/services/movies_api.dart';
 import 'package:bloc/bloc.dart';
 
 part 'home_states.dart';
 
 class HomeCubit extends Cubit<HomeStates> {
   HomeCubit() : super(HomeInitial());
+  final HomeService homeService = HomeService();
+
+  Future<void> getMovies() async {
+    emit(HomeLoading());
+    try {
+      final movies = await homeService.fetchMovies();
+      emit(HomeSuccess(movies));
+    } catch(e) {
+      emit(HomeError('Não foi possível carregar a lista de filmes!'));
+    }
+  }
 
 }
--- lib/logic/cubit/home_states.dart
+++ lib/logic/cubit/home_states.dart
@@ -6,6 +6,14 @@ class HomeInitial extends HomeStates {}
 
 class HomeLoading extends HomeStates {}
 
-class HomeSuccess extends HomeStates {}
+class HomeSuccess extends HomeStates {
+  HomeSuccess(this.movies);
 
-class HomeError extends HomeStates {}
+  final List<Movie> movies;
+}
+
+class HomeError extends HomeStates {
+  HomeError(this.error);
+
+  final String error;
+}
