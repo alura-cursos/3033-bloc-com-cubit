a5397c93723e59737372c9be6ef1a49726d25bb5
ikyrie <mah.c5r@gmail.com>
2023-05-10 11:59:06 -0300

    video2.3

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,10 @@
+import 'package:bilheteria_panucci/logic/cubit/home_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:bilheteria_panucci/components/classification.dart';
 import 'package:bilheteria_panucci/components/home/genre_filter.dart';
 import 'package:bilheteria_panucci/components/movie_card.dart';
 import 'package:bilheteria_panucci/models/movie.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,6 +14,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final HomeCubit homeCubit = HomeCubit();
 
   @override
   void initState() {
@@ -33,16 +36,36 @@ class _HomeState extends State<Home> {
                 ),
               ),
               const GenreFilter(),
-              SliverGrid.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16,
-                  mainAxisExtent: 240,
-                ),
-                itemBuilder: (context, index) {
-                  return MovieCard(movie: Movie(name: "James Bond", classification: Classification.naoRecomendado12, duration: "1h 22min", sinopse: "James Bond é um agente", genre: "Suspense", imageURI: null, sessions: ["18:00"]));
+              BlocBuilder<HomeCubit, HomeStates>(
+                bloc: homeCubit,
+                builder: (context, state) {
+                  if(state == HomeLoading) {
+                    return SliverFillRemaining(child: Center(child: CircularProgressIndicator(),));
+                  } else if(state == HomeSuccess) {
+                    return SliverGrid.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 16,
+                        mainAxisExtent: 240,
+                      ),
+                      itemBuilder: (context, index) {
+                        return MovieCard(
+                            movie: Movie(
+                                name: "James Bond",
+                                classification: Classification.naoRecomendado12,
+                                duration: "1h 22min",
+                                sinopse: "James Bond é um agente",
+                                genre: "Suspense",
+                                imageURI: null,
+                                sessions: ["18:00"]));
+                      },
+                      itemCount: 5,
+                    );
+                  } else {
+                    return SliverFillRemaining(child: Center(child: Text('Deu erro'),));
+                  }
                 },
-                itemCount: 5,
               ),
             ],
           ),
