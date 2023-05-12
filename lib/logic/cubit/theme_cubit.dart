064e5eb3ebfc9461ca5cbdcd6c0f192b1ef01f90
ikyrie <mah.c5r@gmail.com>
2023-05-12 11:03:32 -0300

    video4.1

---
+++ lib/logic/cubit/theme_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:bilheteria_panucci/themes/my_themes.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+class ThemeCubit extends Cubit<ThemeData> {
+  ThemeCubit() : super(MyThemes.getTheme("default"));
+}
